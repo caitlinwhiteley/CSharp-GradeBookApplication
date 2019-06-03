d14b4e1905283926d0ff40849fc71d007f4bfea5
Caitlin <caitlin.whiteleynationwide.co.uk@CaitlinWhiteleynationwidecouks-MacBook-Pro.local>
2019-06-03 10:42:58 +0100

    Completed the module

---
+++ GradeBook/GradeBooks/RankedGradebook.cs
@@ -0,0 +1,12 @@
+﻿using GradeBook.Enums;
+
+namespace GradeBook.GradeBooks
+{
+    public class RankedGradebook : BaseGradeBook
+    {
+        public RankedGradebook(string name) : base(name)
+        {
+            Type = GradeBookType.Ranked;
+        }
+    }
+}
